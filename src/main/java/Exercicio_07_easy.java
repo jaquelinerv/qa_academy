96bd267292b1592fb6499120410da1aba96303ca
Jaqueline <jaquelinevaz95@gmail.com>
2022-07-22 00:46:41 -0300

    changes on Exercicio_07_easy.java

--- src/main/java/Exercicio_07_easy.java
+++ src/main/java/Exercicio_07_easy.java
@@ -4,15 +4,19 @@ public class Exercicio_07_easy {
     static double salario, inss;
 
     public static void main(String[] args) {
-    salario = Double.parseDouble(JOptionPane.showInputDialog("Insira seu salário:"));
+        salario = Double.parseDouble(JOptionPane.showInputDialog("Digite seu salário: "));
 
-
-    if (salario <= 1045.00) {
-        inss = salario * 0.75;
-    }
+        if (salario <= 1045.00) {
+            inss = salario * 0.075;
+        } else if (salario >= 1045.01 && salario <= 2089.60) {
+            inss = salario * 0.09;
+        } else if (salario >= 2089.61 && salario <= 3134.40) {
+            inss = salario * 0.12;
+        } else if (salario >= 3134.41 && salario <= 6101.06) {
+            inss = salario * 0.14;
+        } else {
+            inss = salario * 0.14;
+        }
         System.out.println("O valor a ser pago de INSS é " + inss);
     }
 }
-
-
-
