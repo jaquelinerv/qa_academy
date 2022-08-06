fd46036831e6bde3b08cccd1217e7abbc362ea28
jaquelinerv <jaquelinevaz95@gmail.com>
2022-08-06 16:44:26 -0300

    exercice 05 medium

---
+++ src/main/java/medium/Exercicios_05_medium.java
@@ -0,0 +1,21 @@
+package medium;
+
+import javax.swing.*;
+
+public class Exercicios_05_medium {
+    static int numeroDigitado, i = 0, menorNumero;
+    public static void main(String[] args) {
+        while (i < 5){
+            numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+            if (i == 0){
+                menorNumero = numeroDigitado;
+            }
+            if (numeroDigitado < menorNumero){
+                menorNumero = numeroDigitado;
+            }
+            i++;
+        }
+        System.out.println("O menor número é: " + menorNumero );
+
+    }
+}
