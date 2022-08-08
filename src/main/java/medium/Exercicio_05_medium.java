6a0d6e61159d91181967a44b2d6db5b010f95e72
jaquelinerv <jaquelinevaz95@gmail.com>
2022-08-07 23:20:38 -0300

    exercices 05 and 06

--- src/main/java/medium/Exercicio_05_medium.java
+++ src/main/java/medium/Exercicio_05_medium.java
@@ -1,11 +1,11 @@
 package medium;
-
 import javax.swing.*;
 
-public class Exercicios_05_medium {
+public class Exercicio_05_medium {
     static int numeroDigitado, i = 0, menorNumero;
+
     public static void main(String[] args) {
-        while (i < 5){
+        while(i < 5){
             numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
             if (i == 0){
                 menorNumero = numeroDigitado;
@@ -15,7 +15,12 @@ public class Exercicios_05_medium {
             }
             i++;
         }
-        System.out.println("O menor número é: " + menorNumero );
+        System.out.println("O menor número é: " + menorNumero);
+
 
     }
+
+
+
+
 }
---
+++ src/main/java/medium/Exercicio_06_medium.java
@@ -0,0 +1,21 @@
+package medium;
+
+import org.w3c.dom.ls.LSOutput;
+
+import javax.swing.*;
+
+public class Exercicio_06_medium {
+    static int numeroDigitado, resultadoFatorial = 1;
+    public static void main(String[] args) {
+        numeroDigitado = Integer.parseInt(JOptionPane.showInputDialog("Digite um número: "));
+
+        while (numeroDigitado >= 1){
+            resultadoFatorial = numeroDigitado * resultadoFatorial;
+            numeroDigitado--;
+        }
+
+
+        System.out.println("O resultado da sua fatoração é : " + resultadoFatorial);
+    }
+
+}
